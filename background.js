7f7dc3150dcd70ce73fca9375c5c9825daec9e88
鐵坊家具 <feistoreboss@MacBook-Pro.local>
2026-02-07 16:16:23 +0800

    Update: New GAS Web App URL for AB column support

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 // === 固定設定 ===
-const GAS_URL = "https://script.google.com/a/macros/feistore.com.tw/s/AKfycbyPo3cfKEuYOfjNHmjylWZzKFcqzru5qv6FGwt26Q-UGURhx1ulZP06-0UbzAyJc0LW/exec";
+const GAS_URL = "https://script.google.com/a/macros/feistore.com.tw/s/AKfycbyDecR1DPPuvchcKGLglhT0NofFn4yBOXbyIKtO9TNSD36PdrsAqpHT_GZx3svD4F8t/exec";
 const TOKEN = "FEISTORE_SHOPEE_EXPORT_V1_9fA3kQ7LxP2M6dR8WbZC";
 
 async function postToGAS(payload) {
